eaaf6ccd2314770c233148428317a207295b3215
RhazzXIX <jm21tonio@gmail.com>
2023-05-14 16:17:15 +0800

    Add initial project data

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,52 @@
 import React from "react";
 import "../styles/Projects.css";
+import picSrc from "../assets/images/pBG.jpg";
 
 export default function Projects() {
-  const projects = [];
+  const projects = [
+    {
+      imgSrc: picSrc,
+      name: "Photo-tagging-app",
+      description:
+        'A "Where is Waldo" like App, where you need to look for a list of characters on the image. A project using firebase as a backend service.',
+      id: 1,
+    },
+    {
+      imgSrc: picSrc,
+      name: "Shopping cart",
+      description:
+        "An E-commerce project. Used DummyJSON API as a simulation for backend communication. React-router-dom for a client side routing",
+      id: 2,
+    },
+    {
+      imgSrc: picSrc,
+      name: "Memory Card",
+      description:
+        "A game app where to test your memory. Utilizing React Library functional components.",
+      id: 3,
+    },
+    {
+      imgSrc: picSrc,
+      name: "Battleship",
+      description:
+        'A game app based on the board game "Battleship". Created using only Javascript, HTML & CSS',
+      id: 4,
+    },
+    {
+      imgSrc: picSrc,
+      name: "Weather App",
+      description:
+        'An app that shows the current weather of the entered. Portrays my ability to communicate with the backend and handling "Promises"',
+      id: 5,
+    },
+    {
+      imgSrc: picSrc,
+      name: "Restaurant",
+      description:
+        "A Restaurant page. A project showcasing webpack, ES6 Modules",
+      id: 6,
+    },
+  ];
 
   return (
     <>
