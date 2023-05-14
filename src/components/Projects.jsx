515fd02b69765c3de9ab4f9afde7c326bf75d934
RhazzXIX <jm21tonio@gmail.com>
2023-05-14 15:42:51 +0800

    Add initial JSX to Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,23 @@
-import React from "react"
-import '../styles/Projects.css'
+import React from "react";
+import "../styles/Projects.css";
 
-export default function Projects () {
-  return(
-   <>
-    <h2>Projects</h2>
-   </>
-  )
+export default function Projects() {
+  const projects = [];
+
+  return (
+    <>
+      <h2>Projects</h2>
+      <ul className="cardContainer">
+        {projects.map((project) => {
+          return (
+            <li className="card" key={project.id}>
+              <img src={project.imgSrc} />
+              <h3>{project.name}</h3>
+              <p>{project.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
